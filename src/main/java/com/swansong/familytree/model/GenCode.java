393182706feca0dbf44ae450856aebea7dcda834
grantswanson <31863827+grantswanson@users.noreply.github.com>
2023-03-27 10:04:25 -0700

    Added test cases for GenGode

--- src/main/java/com/swansong/familytree/model/GenCode.java
+++ src/main/java/com/swansong/familytree/model/GenCode.java
@@ -1,11 +1,16 @@
 package com.swansong.familytree.model;
 
 public class GenCode {
-    private GenCode() {}
+    private GenCode() {
+    }
 
     public static String buildSelfCode(String genCode) {
         // remove numbers (multiple marriages),
         return removeLastNumber(genCode);
+    }
+
+    public static String buildSpousesCode(String genCode) {
+        return addLastNumber(genCode);
     }
 
     public static String buildParent1Code(String genCode) {
@@ -14,19 +19,17 @@ public class GenCode {
         // e.g. SA2A1 becomes SA
         return removeLastNumber(removeLastChar(removeLastNumber(genCode)));
     }
+
     public static String buildParent2Code(String genCode) {
         // remove the number and char to indicate current generation.
         // add a number if there is not already one there
         // e.g. SA2A1 becomes SA2
         return addLastNumber(removeLastChar(removeLastNumber(genCode)));
     }
-    public static String buildSpousesCode(String genCode) {
-        return addLastNumber(genCode);
-    }
+
     public static String buildChildsCode(String genCode, int childNumber) {
         return appendLetter(genCode, childNumber);
     }
-
     public static int getChildNumber(String genCode) {
         return getLastLetterValue(removeLastNumber(genCode));
     }
@@ -56,7 +59,6 @@ public class GenCode {
         }
         return s;
     }
-
     private static String removeLastChar(String s) {
         if (s != null && !s.isEmpty()) {
             return s.substring(0, s.length() - 1);
--- src/test/java/com/swansong/familytree/GenCodeTest.java
+++ src/test/java/com/swansong/familytree/GenCodeTest.java
@@ -2,11 +2,17 @@ package com.swansong.familytree;
 
 import com.swansong.familytree.model.GenCode;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Collections;
 import java.util.Map;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class GenCodeTest {
 
@@ -22,7 +28,7 @@ public class GenCodeTest {
                 "SA2A", "SA2A"
         );
         for (String input : cases.keySet()) {
-            String output  = GenCode.buildSelfCode(input);
+            String output = GenCode.buildSelfCode(input);
             assertEquals(cases.get(input), output);
         }
     }
@@ -38,10 +44,11 @@ public class GenCodeTest {
                 "SA2A", "SA2A1"
         );
         for (String input : cases.keySet()) {
-            String output  = GenCode.buildSpousesCode(input);
+            String output = GenCode.buildSpousesCode(input);
             assertEquals(cases.get(input), output);
         }
     }
+
     @Test
     void buildParent1CodeTest() {
         // input, expected output
@@ -59,26 +66,36 @@ public class GenCodeTest {
         }
     }
 
-    @Test
-    void buildParent2CodeTest() {
-        // input, expected output
-        Map<String, String> cases = Map.of(
-                "S", "",
-                "SF", "S1",
-                "SA1", "SA1parent",
-                "SBB2", "SBB2parent",
-                "SBBC", "SBB1",
-                "SBB3C", "SBB3",
-                "SBB2C2", "SBB2C2parent",
-                "SBB2AC2", "SBB2AC2parent"
-        );
-        for (String input : cases.keySet()) {
-            String output = GenCode.buildParent2Code(input);
-            assertEquals(cases.get(input), output);
-        }
+    @ParameterizedTest(name = "{index} buildParent2CodeTest input:{0} expected:{1}")
+    @MethodSource("buildParent2CodeData")
+    void buildParent2CodeTest(String input, String expected) {
+
+        String output = GenCode.buildParent2Code(input);
+        assertEquals(expected, output);
     }
+
+    static Stream<Arguments> buildParent2CodeData() {
+        return Stream.of(    // input, expected output
+                Arguments.of("S", ""),
+                Arguments.of("SF", "S1"),
+                Arguments.of("SA1", "S1"),
+                Arguments.of("SBB2", "SB1"),
+                Arguments.of("SBBC", "SBB1"),
+                Arguments.of("SBB3C", "SBB3"),
+                Arguments.of("SBB2C2", "SBB2")
+        );
+    }
+
+    @ParameterizedTest
+    @CsvSource({"S, 1, SA", "SABC, 2, SABCB", "SA1B2C1, 3, SA1B2C1C", " ,1,A"})
+    void buildChildsCodeTest(String genCode, int num, String expected) {
+        String output = GenCode.buildChildsCode(genCode, num);
+        assertEquals(expected, output);
+
+    }
+
     @Test
-    void buildChildsCodeTest() {
+    void buildChildsCodeTest2() {
         // input, expected output
         Map<Map<String, Integer>, String> cases = Map.of(
                 Collections.singletonMap("S", 1), "SA",
@@ -92,5 +109,26 @@ public class GenCodeTest {
             String output = GenCode.buildChildsCode(input.getKey(), input.getValue());
             assertEquals(cases.get(singletonMap), output);
         }
+    }
+
+    @ParameterizedTest
+    @CsvSource({"SA, 1", "SB,2", "SA1, 1", "SA2, 1", "SAAAC1, 3", ",-1", "333, -1"})
+    void getChildNumberTest(String genCode, int expected) {
+        assertEquals(expected, GenCode.getChildNumber(genCode));
+    }
+
+    @Test
+    void buildParent1CodeExceptionTest() {
+        assertThrows(RuntimeException.class, () -> GenCode.buildParent1Code(null));
+        assertThrows(RuntimeException.class, () -> GenCode.buildParent1Code(""));
+    }
+
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    @Test
+    void buildChildsCodeExceptionTest() {
+        assertThrows(IllegalArgumentException.class, () -> GenCode.buildChildsCode("SA", 27));
+        assertThrows(IllegalArgumentException.class, () -> GenCode.buildChildsCode("SA", 54982));
+        assertThrows(IllegalArgumentException.class, () -> GenCode.buildChildsCode("SA", 0));
+        assertThrows(IllegalArgumentException.class, () -> GenCode.buildChildsCode("SA", -3));
     }
 }

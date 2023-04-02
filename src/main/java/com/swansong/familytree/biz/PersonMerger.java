cf04ebb40ff23fc99d66ce0d27033ea40ac2dff6
grantswanson <31863827+grantswanson@users.noreply.github.com>
2023-04-02 00:59:36 -0700

    Remove unused code

--- src/main/java/com/swansong/familytree/biz/PersonMerger.java
+++ src/main/java/com/swansong/familytree/biz/PersonMerger.java
@@ -1,20 +1,12 @@
 package com.swansong.familytree.biz;
 
-import com.swansong.familytree.model.Marriage;
 import com.swansong.familytree.model.Name;
 import com.swansong.familytree.model.Person;
 
-import java.util.ArrayList;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Set;
-
 public class PersonMerger {
 
-    public static List<String> failedMerges = new ArrayList<>();
-    public static List<String> similarPersonMerges = new ArrayList<>();
 
-
+    @SuppressWarnings({"unused", "UnusedAssignment"})
     public static boolean merge(Person person, Name altName, int rowNum, String altNameSource) {
         if (person == null) {
             throw new RuntimeException("ln#:" + rowNum +
@@ -52,50 +44,9 @@ public class PersonMerger {
             String s = "Merge failed ln#:" + rowNum + " Source:" + altNameSource +
                     "\n name from row text:'" + altName.getLastCommaFirst() + "'" +
                     "\n name from genCode :'" + person.getName().getLastCommaFirst() + "' " + person.getGenCode();
-            failedMerges.add(s);
             System.out.println(s);
             return false;
         }
     }
 
-    private static Name findRightName(Person person, Name altName) {
-        Set<Name> myselfNames = getNameOfMyselfFromChildrensParent(person);
-        for (Name myselfName : myselfNames) {
-            if (Name.isMergeAllowed(myselfName, person.getName())) {
-                System.out.println("Found right name:" + myselfName);
-                //return myselfName;
-            } else {
-                System.out.println("Did NOT find right name:" + myselfName);
-            }
-            if (Name.isMergeAllowed(myselfName, altName)) {
-                System.out.println("Found right name:" + myselfName);
-                //return myselfName;
-
-            } else {
-                System.out.println("Did NOT find right name:" + myselfName);
-            }
-        }
-        return null;
-    }
-
-    private static Set<Name> getNameOfMyselfFromChildrensParent(Person person) {
-        Set<Name> names = new LinkedHashSet<>();
-        List<Marriage> marriages = person.getMarriages();
-        for (Marriage marriage : marriages) {
-            Person[] children = marriage.getChildren();
-            for (Person child : children) {
-                if (child != null) {
-                    Person father = child.getFather();
-                    if (father != null) {
-                        names.add(father.getName());
-                    }
-                    Person mother = child.getMother();
-                    if (mother != null) {
-                        names.add(mother.getName());
-                    }
-                }
-            }
-        }
-        return names;
-    }
 }

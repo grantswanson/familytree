678c5be197e3127ac6d0aef43ea0c971b011c0f1
grantswanson <31863827+grantswanson@users.noreply.github.com>
2023-04-04 00:33:18 -0700

    Add unit tests

--- src/test/java/com/swansong/familytree/NameTest.java
+++ src/test/java/com/swansong/familytree/NameTest.java
@@ -16,23 +16,23 @@ import static org.junit.jupiter.api.Assertions.*;
 public class  NameTest {
 
     @Test
-    void parseLastCommaFirstNameTest() {
+    void parseFullNameTest() {
         // input, expected output
         Map<String, String> cases = Map.ofEntries(
                 Map.entry("Robert Joseph, Jr.", ", Robert Joseph, Jr"),
-//                Map.entry("MAGNUSSON, *", "Magnusson, "),
-//                Map.entry(" SWANSON  ,  GRANT * ", "Swanson, Grant"),
-//                Map.entry(" McGee ,  Bill ", "McGee, Bill"),
-//                Map.entry("SWANSON *,   ", "Swanson, "),
-//                Map.entry("  ,  Cynthia", ", Cynthia"),
-//                Map.entry("SWANSON, Carl Loyal William \"Loyal\"", "Swanson, Carl Loyal William \"Loyal\""),
-//                Map.entry(" Swanson  ,  Bill,  Jr. * ", "Swanson, Bill, Jr"),
-//                Map.entry(" Swanson  ,  William \"Billy *\" ,  Jr.   ", "Swanson, William \"Billy\", Jr"),
-//                Map.entry("                 , Sandra", ", Sandra"),
-//                Map.entry(",Jana Lynn [Neuzil*] * ", ", Jana Lynn [Neuzil]"),
-//                Map.entry("Burns, Sandra Kay (Peters)(Lane)", "Burns, Sandra Kay [Peters] [Lane]"),
-//                Map.entry("McGEE, ANDREW SHANE", "McGEE, Andrew Shane"), // fix later
-//                Map.entry(" SWANSON  ,  GRANT * alt: Grantt", "Swanson, Grant alt: Grantt"),
+                Map.entry("MAGNUSSON, *", "Magnusson, "),
+                Map.entry(" SWANSON  ,  GRANT * ", "Swanson, Grant"),
+                Map.entry(" McGee ,  Bill ", "McGee, Bill"),
+                Map.entry("SWANSON *,   ", "Swanson, "),
+                Map.entry("  ,  Cynthia", ", Cynthia"),
+                Map.entry("SWANSON, Carl Loyal William \"Loyal\"", "Swanson, Carl Loyal William \"Loyal\""),
+                Map.entry(" Swanson  ,  Bill,  Jr. * ", "Swanson, Bill, Jr"),
+                Map.entry(" Swanson  ,  William \"Billy *\" ,  Jr.   ", "Swanson, William \"Billy\", Jr"),
+                Map.entry("                 , Sandra", ", Sandra"),
+                Map.entry(",Jana Lynn [Neuzil*] * ", ", Jana Lynn [Neuzil]"),
+                Map.entry("Burns, Sandra Kay (Peters)(Lane)", "Burns, Sandra Kay [Peters] [Lane]"),
+                Map.entry("McGEE, ANDREW SHANE", "McGEE, Andrew Shane"), // fix later
+                Map.entry(" SWANSON  ,  GRANT * alt: Grantt", "Swanson, Grant alt: Grantt"),
                 Map.entry("SWANSON, SVEN WILHELM \"William\"", "Swanson, Sven Wilhelm \"William\"")
         );
 
@@ -47,6 +47,34 @@ public class  NameTest {
     void parseLastCommaFirstNameExceptionTest() {
         assertThrows(IllegalArgumentException.class, () -> Name.parseFullName(null));
         assertThrows(IllegalArgumentException.class, () -> Name.parseFullName("smith, joe, jr, sr"));
+    }
+
+    @Test
+    void toNameKeyTest() {
+        // input, expected output
+        Map<String, String> cases = Map.ofEntries(
+                Map.entry("Robert Joseph, Jr.", ", Robert Joseph, Jr"),
+                Map.entry("MAGNUSSON, *", "Magnusson, "),
+                Map.entry(" SWANSON  ,  GRANT * ", "Swanson, Grant"),
+                Map.entry(" McGee ,  Bill ", "McGee, Bill"),
+                Map.entry("SWANSON *,   ", "Swanson, "),
+                Map.entry("  ,  Cynthia", ", Cynthia"),
+                Map.entry("SWANSON, Carl Loyal William \"Loyal\"", "Swanson, Carl Loyal William"),
+                Map.entry(" Swanson  ,  Bill,  Jr. * ", "Swanson, Bill, Jr"),
+                Map.entry(" Swanson  ,  William \"Billy *\" ,  Jr.   ", "Swanson, William, Jr"),
+                Map.entry("                 , Sandra", ", Sandra"),
+                Map.entry(",Jana Lynn [Neuzil*] * ", ", Jana Lynn"),
+                Map.entry("Burns, Sandra Kay (Peters)(Lane)", "Burns, Sandra Kay"),
+                Map.entry("McGEE, ANDREW SHANE", "McGEE, Andrew Shane"), // fix later
+                Map.entry(" SWANSON  ,  GRANT * alt: Grantt", "Swanson, Grant"),
+                Map.entry("SWANSON, SVEN WILHELM \"William\"", "Swanson, Sven Wilhelm")
+        );
+
+        for (String input : cases.keySet()) {
+            Name name = Name.parseFullName(input);
+            String output = name.toNameKey().toString();
+            assertEquals(cases.get(input), output);
+        }
     }
 
     @ParameterizedTest

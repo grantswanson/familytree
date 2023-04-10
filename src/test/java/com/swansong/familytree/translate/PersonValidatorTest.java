2b3ff9ed707aca9c885b47bfa1c95869a02f0be7
grantswanson <31863827+grantswanson@users.noreply.github.com>
2023-04-10 00:53:08 -0700

    Fixed up unit test case

--- src/test/java/com/swansong/familytree/translate/PersonValidatorTest.java
+++ src/test/java/com/swansong/familytree/translate/PersonValidatorTest.java
@@ -5,27 +5,28 @@ import com.swansong.familytree.model.Person;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PersonValidatorTest {
 
     @ParameterizedTest
     @CsvSource({
-            "2000 Jan 01, 2001 Jan 01, 2002 Jan 01, 2018 Jun 01, 2050 Jan 01, false",
-            "2000 Jan 01, 1999 Jan 01, 2002 Jan 01, 2018 Jun 01, 2050 Jan 01, true",
-            "2000 Jan 01, 2001 Jan 01, 1999 Jan 01, 2018 Jun 01, 2050 Jan 01, true",
-            "2000 Jan 01, 2001 Jan 01, 2002 Jan 01, 1999 Jun 01, 2050 Jan 01, true",
-            "2000 Jan 01, 2001 Jan 01, 2002 Jan 01, 2018 Jun 01, 1999 Jan 01, true",
-            "2000 Jan 01, null     , null     , null     , null     , false",
-            "null     , null     , null     , null     , null     , false"
+            "2000 Jan 01, 2001 Jan 01, 2002 Jan 01, 2018 Jun 01, 2050 Jan 01, 0",
+            "2000 Jan 01, 1999 Jan 01, 2002 Jan 01, 2018 Jun 01, 2050 Jan 01, 1",
+            "2000 Jan 01, 2001 Jan 01, 1999 Jan 01, 2018 Jun 01, 2050 Jan 01, 1",
+            "2000 Jan 01, 2001 Jan 01, 2002 Jan 01, 1999 Jun 01, 2050 Jan 01, 1",
+            "2000 Jan 01, 2001 Jan 01, 2002 Jan 01, 2018 Jun 01, 1999 Jan 01, 4",
+            "2000 Jan 01, null     , null     , null     , null     , 0",
+            "null     , null     , null     , null     , null     , 0"
     })
     void testValidatePersonDates(String dob,
                                  String baptismDate,
                                  String confirmationDate,
                                  String highSchoolGradDate,
                                  String deathDate,
-                                 boolean shouldThrow) {
+                                 int expectedWarningsCount) {
         Person person = new Person();
         person.setName(Name.parseFullName("Doe, John"));
         person.setDob(dob);
@@ -34,11 +35,9 @@ public class PersonValidatorTest {
         person.setHighSchoolGradDate(highSchoolGradDate);
         person.setDeathDate(deathDate);
 
-        if (shouldThrow) {
-            assertThrows(IllegalArgumentException.class,
-                    () -> PersonValidator.getWarnings(person));
-        } else {
-            assertDoesNotThrow(() -> PersonValidator.getWarnings(person));
-        }
+        List<String> warnings = PersonValidator.getWarnings(person);
+        //System.out.println("Warnings:"+warnings);
+        assertEquals(expectedWarningsCount, warnings.size());
+
     }
 }
